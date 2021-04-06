168a07fe660b919c021c17427ad46b6b10f7a4bf
Andrei Jiroh Eugenio Halili <andreijiroh@madebythepins.tk>
2021-04-06 19:41:03 +0800

    heroku-start: Update script

--- heroku_start.sh
+++ heroku_start.sh
@@ -6,22 +6,24 @@ export ROCKET_PORT="${PORT}"
 # TODO: Store rsa_key in db or env 
 export ORG_ATTACHMENT_LIMIT=0
 export USER_ATTACHMENT_LIMIT=0
-export DISABLE_ICON_DOWNLOAD=true
+#export DISABLE_ICON_DOWNLOAD=true
 export ICON_CACHE_TTL=0
 export ICON_CACHE_NEGTTL=0
 
 if [ "${ENABLE_ADMIN}" == "true" ]; then
-  echo "ENABLE ADMIN"
-  export ADMIN_TOKEN="${GEN_ADMIN_TOKEN}"
+  echo "==> Enabling admin dashboard..."
+  if [ "${ADMIN_TOKEN}" == "" ]; then
+    export ADMIN_TOKEN="${GEN_ADMIN_TOKEN}"
+  fi
 else
   echo "DISABLE ADMIN"
   unset ADMIN_TOKEN
 fi
 
 if [ -z "${RSA_CONTENT}" ]; then
-  echo "RSA CONTENT not set"
+  echo "==> RSA_CONTENT is missing, please see https://rtapp.tk/bwrs-rsa-content-env"
 else
-  echo "Restore rsa content"
+  echo "==> RSA_CONTENT found, attempting to decode..."
   echo "${RSA_CONTENT}" | base64 -d | tar zxf -
   export RSA_KEY_FILENAME=${PWD}/rsa_key
 fi
